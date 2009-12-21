0843757f81d7cd440a6194046f3d97c298df8153
Josh Aronson <josh.aronson@skyroo.com>
2009-12-21 16:03:36 +0000

    modify README

--- config/environment.rb
+++ config/environment.rb
@@ -20,8 +20,9 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+	config.gem "rspec"
 	config.gem "friendly"
-	config.gem "googlecharts", :lib => "gchart"
+	#config.gem "googlecharts", :lib => "gchart"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
