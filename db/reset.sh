63153905b4ebf7e0ca40eae7d1039fc888453d0b
Josh Aronson <josh.aronson@skyroo.com>
2009-12-21 15:53:01 +0000

    modify database.example, README

--- db/reset.sh
+++ db/reset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rake --trace db:drop:all
-rake --trace db:create:all
-rake --trace db:migrate
-rake --trace db:seed
+rake --trace db:drop:all RAILS_ENV=$RAILS_ENV
+rake --trace db:create:all RAILS_ENV=$RAILS_ENV
+rake --trace db:migrate RAILS_ENV=$RAILS_ENV
+rake --trace db:seed RAILS_ENV=$RAILS_ENV
