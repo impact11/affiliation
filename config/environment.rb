9f45360df506c0a467633c5f52921d055459653b
Josh Aronson <josh.aronson@skyroo.com>
2009-12-21 17:04:37 +0000

    ?

--- config/environment.rb
+++ config/environment.rb
@@ -20,7 +20,6 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-	config.gem "rspec"
 	config.gem "friendly"
 	#config.gem "googlecharts", :lib => "gchart"
 
